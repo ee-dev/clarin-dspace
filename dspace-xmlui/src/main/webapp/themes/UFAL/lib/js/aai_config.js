f6fa95fd7b6044c077121007f38cddfbf4d9779b
bodl-ota-svc <bodl-ota-svc@ota-qa.bodleian.ox.ac.uk>
2017-03-24 12:08:29 +0000

    repository specific configuration by jm/lindat

--- dspace-xmlui/src/main/webapp/themes/UFAL/lib/js/aai_config.js
+++ dspace-xmlui/src/main/webapp/themes/UFAL/lib/js/aai_config.js
@@ -23,10 +23,10 @@ jQuery(document).ready(
                         instance.host + instance.port + instance.repoPath +
                             "themes/UFAL/lib/html/disco-juice.html?";
             instance.metadataFeed = instance.target + "discojuice/feeds";
-            instance.serviceName = "LINDAT/CLARIN Repository";
+            instance.serviceName = "OTA Repository";
             instance.localauth =
                 '<form method="post" action="' + instance.target + 'password-login"> ' +
-                    '<p>Sign in using your local account obtained from the LINDAT/CLARIN administrator.</p>' +
+                    '<p>Sign in using your local account obtained from OTA.</p>' +
                     '<p style="margin: 5px; color: #888" ><input type="text" name="login_email" style="font-size: 160%; width: 100%" id="login" /> <label for="login">E-Mail Address</label></p>' +
                     '<p style="margin: 5px; color: #888" ><input type="password" name="login_password" style="font-size: 160%; width: 100%" id="pass" /> <label for="pass">Password</label></p>' +
                     '<p style="margin: 5px; color: #607890; text-decoration: underline;"><a href="' + instance.target + 'forgot">Forgot your password?</a></p>' +
@@ -35,9 +35,6 @@ jQuery(document).ready(
             instance.target = instance.target + "shibboleth-login";
             return instance;
         })();
-        if (!("aai" in window)) {
-            throw "Failed to find UFAL AAI object. See https://redmine.ms.mff.cuni.cz/projects/lindat-aai for more details!";
-        }
         window.aai.setup(opts);
     }
 ); // ready
